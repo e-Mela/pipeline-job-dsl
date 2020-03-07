0dbf82b87e0d4c139aaf8906fbb8a9ab8d29ba5f
Achyut Devkota <achyut.dev@gmail.com>
2020-03-07 11:14:45 -0500

    Update job_example_project_build.groovy

--- src/main/groovy/example/job_example_project_build.groovy
+++ src/main/groovy/example/job_example_project_build.groovy
@@ -1,5 +1,5 @@
 def config = new ConfigSlurper().parse(readFileFromWorkspace('src/main/resources/example/application/example-properties.groovy'))
 
-job('invoice-matcher-build') {
+job('example-project-build') {
     label(config.app_name)
 }
