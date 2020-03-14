383f6dae6725b64e8a0fcb1725c68fe7501f3c9f
Achyut Devkota <achyut.dev@gmail.com>
2020-03-14 11:58:25 -0400

    Update job_example_project_build.groovy

--- src/main/groovy/example/job_example_project_build.groovy
+++ src/main/groovy/example/job_example_project_build.groovy
@@ -1,7 +1,7 @@
 def config = new ConfigSlurper().parse(readFileFromWorkspace('src/main/resources/example/application/example-properties.groovy'))
 
 job('example-project-build') {
-    label(config.app_name)
+    label('master')
     scm {
         git {
             remote {
