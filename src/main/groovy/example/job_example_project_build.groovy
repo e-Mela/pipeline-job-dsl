08c7f5cb49a66889b7088a04b0255036418ab2c4
achyutdev <achyut.dev@gmail.com>
2020-03-28 09:08:20 -0400

    adding new pr job

--- src/main/groovy/example/job_example_project_build.groovy
+++ src/main/groovy/example/job_example_project_build.groovy
@@ -1,5 +1,34 @@
 def config = new ConfigSlurper().parse(readFileFromWorkspace('src/main/resources/example/application/example-properties.groovy'))
 
+job('example-project-pull-request-builder') {
+    label('master')
+    scm {
+        git {
+            remote {
+                github('e-Mela/sample-project', 'https')
+                credentials('github_credential')
+            }
+            branch(config.git.project.branch)
+        }
+    }
+    wrappers {
+        timestamps()
+    }
+    steps {
+        environmentVariables {
+            propertiesFile('gradle.properties')
+        }
+        gradle('clean build')
+    }
+    triggers {
+        githubPush()
+    }
+
+    publishers {
+
+    }
+}
+
 job('example-project-build') {
     label('master')
     scm {
